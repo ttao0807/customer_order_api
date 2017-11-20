c979df994cd75a33c21ec1cfcdfee88bba725f0e
ttao0807 <ttao0807@gmail.com>
2017-11-20 05:41:52 -0500

    Customers moel created

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,16 @@ module CustomerOrderApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
